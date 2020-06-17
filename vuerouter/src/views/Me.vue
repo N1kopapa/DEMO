eec9b99e8db12745501892e187fb9e5fdb63d81f
n1kopapa <1293165064@qq.com>
2020-06-18 00:07:47 +0800

    1

---
+++ vuerouter/src/views/Me.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span>this is new page</span>
+    <div @click="show">show</div>
+  </div>
+</template>
+
+<script>
+export default {
+    methods:{
+        show(){
+            console.log(this)
+        }
+    }
+}
+</script>
